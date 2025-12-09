6f3b513ee3c3dac0d91ecda9034bf6cfd8648a77
Tonin <152934527+antoninho067@users.noreply.github.com>
2025-12-09 09:32:48 -0400

    Add files via upload

---
+++ src/pages/noticia/[id].js
@@ -0,0 +1,20 @@
+import { Container } from 'react-bootstrap'
+import Cards from '../components/cards'
+export async function getServerSideProps(req) {
+    // Fetch data from  API notícias
+    const res = await fetch('https://apinoticias6b.vercel.app/noticias/id/' + req.query.id)
+    const repo = await res.json()
+    // Pass data to the page via props
+    return { props: { noticia: repo } }
+}
+export default function Page({ noticia }) {
+    return <>
+        <Container className="text-center">
+            <Cards idnoticia={noticia[0]._id}
+                conteudonoticia={noticia[0].conteudonoticia}
+                titulonoticia={noticia[0].titulonoticia}
+                datahoracadastro={noticia[0].datahoracadastro}
+                tiponoticia={noticia[0].tiponoticia} />
+        </Container>
+    </>
+}
